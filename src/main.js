f0e6d68f9f36e3ff63d106bc34607668e10b0509
Fauzzzs7 <fauzsyabana68@gmail.com>
2023-07-02 16:08:36 +0700

    login&signup

--- src/main.js
+++ src/main.js
@@ -1,15 +1,6 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import router from "./router";
+import "./style.css";
+import App from "./App.vue";
 
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import "swiper/css/effect-coverflow";
-
-
-
-createApp(App)
-.use(router)
-.mount('#app')
+createApp(App).use(router).mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomePages from '../pages/HomePages.vue'
-import DetailContent from '../pages/DetailContent.vue'
-
+import { createRouter, createWebHistory } from "vue-router";
+import login from "../pages/login.vue";
+import signup from "../pages/signup.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomePages
+      path: "/",
+      name: "login",
+      component: login,
     },
     {
-        path: '/detail',
-        name: 'detail',
-        component: DetailContent
-      },
-    
-  ]
-})
+      path: "/signup",
+      name: "sign",
+      component: signup,
+    },
+  ],
+});
 
-export default router
+export default router;
